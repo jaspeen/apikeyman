6d063f8dacdf52fe13dc5d858769c27809f443d1
Artem Mironov <jaspeen@gmail.com>
2024-05-23 13:06:53 +0300

    Remove non pages content

--- algo/all/all.go
+++
@@ -1,9 +0,0 @@
-package all
-
-// Import this file for side-effect to register all algorithms
-import (
-	_ "github.com/jaspeen/apikeyman/algo/ecdsa"
-	_ "github.com/jaspeen/apikeyman/algo/eddsa"
-	_ "github.com/jaspeen/apikeyman/algo/rsa"
-	_ "github.com/jaspeen/apikeyman/algo/secp256k1"
-)
--- algo/errors.go
+++
@@ -1,8 +0,0 @@
-package algo
-
-import "errors"
-
-var ErrInvalidKeyType = errors.New("invalid key type")
-var ErrKeyMustBePEMEncoded = errors.New("key must be PEM encoded")
-var ErrHashUnavailable = errors.New("hash function not available")
-var ErrInvalidSignature = errors.New("invalid signature")
